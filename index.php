eb3418344aa3e0b61569e5d026a43ba035222f18
Braedon Billingsley <Billingsley.Braedon@student.greenriver.edu>
2024-01-20 20:14:12 -0500

    Turned on error reporting, required autoload file, instantiated Fat-Free framework 'F3', defined a default route to views/home.html, echoed 'My Application'

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Braedon Billingsley
+ * 01/20/24
+ * https://www.bbillingsley.greenriverdev.com/home/bbilling/public_html/328/application
+ * This is my CONTROLLER for the application app.
+ */
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require the autoload file.
+require_once('vendor/autoload.php');
+
+//Instantiate Fat-Free framework (F3)
+$f3 = Base::instance(); // :: is invoking a static method in php
+
+// Define a default route.
+$f3->route('GET /', function () { // Anonymous function
+    echo "My Application";
+
+    // Display a view page
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+// Run Fat-Free
+$f3->run(); // -> Invokes instance method in php
+
