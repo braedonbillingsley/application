bcc0c39bde457b4c8ccf12f3ebfa58967eda0a0d
Braedon Billingsley <Billingsley.Braedon@student.greenriver.edu>
2024-02-05 01:04:50 -0500

    Added f3 form data and fixed routes

--- index.php
+++ index.php
@@ -16,42 +16,66 @@ require_once('vendor/autoload.php');
 //Instantiate Fat-Free framework (F3)
 $f3 = Base::instance(); // :: is invoking a static method in php
 
-// Define a default route.
-$f3->route('GET /', function () { // Anonymous function
-    // Display a view page
+// function to process form data
+function processFormData($f3, $formFields, $redirect)
+{
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        // Store form data in the F3 hive
+        foreach ($formFields as $field) {
+            $f3->set($field, $_POST[$field]);
+        }
+
+        $f3->reroute($redirect);
+    }
+}
+
+//default route.
+$f3->route('GET /', function () {
     $view = new Template();
     echo $view->render('views/home.html');
-
 });
 
-$f3->route('GET|POST /info', function ($f3) { // Anonymous function
+// Route for personal info
+$f3->route('GET|POST /info', function ($f3) {
+    processFormData($f3, ['first_name', 'last_name', 'email', 'state', 'phone'], '/experience');
 
-    // If the form has been posted
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-        $f3->reroute('/experience');
-
-        // TODO store in session, redirect to mail, then summary while storing user data in session.
-    }
-    // Display a view page
     $view = new Template();
     echo $view->render('views/personal-info.html');
 });
 
-$f3->route('GET|POST /experience', function () { // Anonymous function
+// Route for experience
+$f3->route('GET|POST /experience', function ($f3) {
+    processFormData($f3, ['bio', 'github', 'experience', 'relocate'], '/mail');
 
-    // Display a view page
     $view = new Template();
     echo $view->render('views/experience.html');
-
 });
 
-$f3->route('GET|POST /mail', function () { // Anonymous function
-    // Display a view page
+// Route for mail
+$f3->route('GET|POST /mail', function ($f3) {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        $selected_technologies = array_filter($_POST, function($key) {
+            return str_starts_with($key, 'tech-');
+        }, ARRAY_FILTER_USE_KEY);
+
+        $selected_industries = array_filter($_POST, function($key) {
+            return str_starts_with($key, 'ind-');
+        }, ARRAY_FILTER_USE_KEY);
+
+        $f3->set('selected_technologies', $selected_technologies);
+        $f3->set('selected_industries', $selected_industries);
+
+        $f3->reroute('/summary');
+    }
+
     $view = new Template();
     echo $view->render('views/mail.html');
-
 });
-// Run Fat-Free
-$f3->run(); // -> Invokes instance method in php
 
+$f3->route('GET|POST /summary', function($f3) {
+    $view = new Template();
+    echo $view->render('views/summary.html');
+});
+
+$f3->run();
